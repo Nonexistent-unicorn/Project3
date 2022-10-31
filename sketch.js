217ff4d958d7b36087c5f55336eed02f38391f80
Olivia Stone <ods20a@fsu.edu>
2022-10-31 13:57:34 -0400

    added color controls, notes and got rid of cursor

--- sketch.js
+++ sketch.js
@@ -1,24 +1,28 @@
-//Pollock Splatter Zen by kwertyops
-    //Basic code for paint splatters
-//might do something else
-/*To Do:
-  -add more colors
-  -change variables in splatter function
-  -other things to try:
-      -ribbons
-      -exploding shapes
-      -click to add colored ripple/pulse */
+/*Pollock Splatter Zen by kwertyops
+    Basic code for paint splatters
+-this is my main terminal, I am experimenting in another terminal and adding what works here
 
+To Do:
+  more control for colors
+  Click funtion for brush strokes (only paint when clicked)
+  */
+let colorPicker;
 let colors = [];
 let brush = { x:0, y:0, px:0, py:0}
 let seed;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+  colorPicker = createColorPicker('#000000')
+  colorPicker2 = createColorPicker('#000000')
+  colorPicker3 = createColorPicker('#000000')
+  colorPicker4 = createColorPicker('#000000')
+  colorPicker5 = createColorPicker('#000000')
+  noCursor()
 	noStroke();
 	seed = random(1000);
 	colors = [
-	color(251,248,204), 
+  color(251,248,204), 
 	color(253,228,207), 
 	color(255,207,210), 
 	color(241,192,232), 
@@ -28,13 +32,14 @@ function setup() {
   color(142,236,245),
   color(152,245,225),
   color(185,251,192),
-   ]
+ ]
 	let base = floor(random(colors.length));
 	background(colors[base]);
 	colors.splice(base,1);
 }
 
 function draw() {
+  
 	brush.x+=(mouseX-brush.x)/12;
 	brush.y+=(mouseY-brush.y)/12;
 	if(frameCount>40){
@@ -48,7 +53,7 @@ function mouseMoved(){
 	if(frameCount%7==0){
 		splatter(mouseX, mouseY);
 		//splatter(width-mouseY, height-mouseX);
-		stipple(mouseX, mouseY, 0);
+		stipple(mouseX, mouseY, colorPicker.color());
 		//stipple(width-mouseX, height-mouseY, 255);
 	}
 }
@@ -57,18 +62,18 @@ function drizzle(){
 	let s = 1+30/dist(brush.px, brush.py, brush.x, brush.y);
 	s=min(20,s);
 	strokeWeight(s);
-	stroke(0);
+	stroke(colorPicker2.color());
 	line(brush.px, brush.py, brush.x, brush.y);
-	stroke(0);
+	stroke(colorPicker3.color());
 	//line(width-brush.px, height-brush.py, width-brush.x, height-brush.y);
 }
 
 function splatter(bx,by){
 	let c = colors[floor(random(colors.length))];
-	bx += random(-15,100);
-	by += random(-15,100);
-	let mx = 10*movedX;
-	let my = 10*movedY;
+	bx += random(-15,200);
+	by += random(-15,200);
+	let mx = 50*movedX;
+	let my = 50*movedY;
 	for(let i=0; i<80; i++){
 		seed+=.01;
 		let x = bx+mx*(0.5-noise(seed+i));
